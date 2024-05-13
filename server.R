a56f922974cb3bed004314e04d16942251838ada
kgzet <164067705+kgzet@users.noreply.github.com>
2024-05-14 01:19:29 +0200

    server logic for plotting gene expression

--- server.R
+++ server.R
@@ -9,33 +9,36 @@ library(tidyr)
 
 # loading data from a file
 Table_TPMs<-readr::read_csv("data/TPMs_table_100genes.csv")
-Table_TPMs
 TPMs_Genes<-Table_TPMs %>% 
-  dplyr::select("GeneID")
-TPMs_Genes
-c(TPMs_Genes)[1]
+  dplyr::select("GeneID")  # labels for UI
+#c(TPMs_Genes)[1]
+
+# generating data frame with values moved to a specific column
+Table_TPMs_long<-pivot_longer(data=Table_TPMs, 
+                              cols=colnames(Table_TPMs)[-1],# Cols to pivot: all except 1st
+                              values_to = "TPMs",# name of the col that contained the vals of pivoted cols
+                              names_to = "Sample")# name of the col that contains the names of the pivoted cols
 
 # Define server logic required to plot the expression in TPMs
-
 
 
 function(input, output, session) {
 
     output$distPlot <- renderPlot({
       
-      updateSelectInput(session, inputId = "select_gene", choices = )
+     # updateSelectInput(session, inputId = "select_gene")
       
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2]
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white',
-           xlab = 'Waiting time to next eruption (in mins)',
-           main = 'Histogram of waiting times')
+      # gene name selected by user in UI selectInput
+      gene_name <- input$select_gene
+      gene_selected <- Table_TPMs_long
+      # selecting specific rows in dataframe, containing gene name chosen by user
+      gene_to_plot <- gene_selected[gene_selected$GeneID == gene_name,]
+      # plot data for one specific gene chosen by user
+      # dataframe is rows containing keyword - gene name
+      # geom_point() plots data as single points
+      ggplot(data = gene_to_plot, aes(x=Sample, y=TPMs, col = 'lightpink')) + geom_point()
 
     })
-
 }
 
 # No AI used.
